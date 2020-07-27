111e50bab2095ebe30b6af43ade8de6346581b85
AdamKong <fykong@163.com>
2020-07-27 20:24:54 +0800

    in feat-mean br

--- com/adamkong/test/HelloWorld.java
+++ com/adamkong/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World!");
         System.out.println("Hello Git and IntelliJ!");
+        System.out.println("Hello, this is feat-mean branch!");
     }
 }
